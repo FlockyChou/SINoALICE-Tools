7966b8dd8920f5f97ef947df00581a67611a4923
FlockyChou <flockychou@gmail.com>
2020-08-09 20:48:56 -0400

    Added All Acts and Chapters
    I don't think this is going to work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 $(function() {
+  const min_ap = 6;
+
   let missions;
 
   $.getJSON('/data/missions.json', function(data) {
+    // Set JSON data to variable
     missions = data;
   }).done(function() {
+    // Sort missions by experience ratio from highest to lowest
     missions = missions.sort(function(key_1, key_2) {
       return -(key_1.experience_ratio - key_2.experience_ratio);
     });
@@ -24,13 +28,8 @@ $(function() {
     let remaining_exp = $max_exp.val() - $current_exp.val();
     let remaining_ap  = $ap.val();
 
-    let mission_list = [];
-    let exp_list     = [];
-    let ap_list      = [];
-
-    // Used later on to track what option wastes the least amount of AP.
-    let lowest_ap  = remaining_ap;
-    let lowest_exp = remaining_exp;
+    let exp_earned    = 0;
+    let ap_used       = 0;
 
     $tbody.append(`
       <tr>
@@ -41,36 +40,51 @@ $(function() {
       </tr>
     `);
 
-    while(remaining_exp > 0) {
+    let loops = 100
+
+    while(remaining_exp > 0 && loops > 0) {
       $.each(missions, function(index, mission) {
         // Skip if AP cost is too high
         if(mission.ap_cost > remaining_ap) { return true; }
-        // Skip if experience earned is greater than remaining
+
+        // Skip if EXP gain > 25% of the remaining EXP because magic numbers
+        if(mission.chapter != 1 && mission.experience_earned >= remaining_exp * 0.25) { return true; }
 
         let temp_remaining_exp = remaining_exp - mission.experience_earned;
         let temp_remaining_ap  = remaining_ap  - mission.ap_cost;
 
-        if(0 > temp_remaining_exp) {
-          if(lowest_ap > temp_remaining_ap) {
-            lowest_ap = temp_remaining_ap;
-            return true;
-          }
-        }
+        // Skip if experience earned is greater than remaining
+        if(0 > remaining_exp) { return true; }
 
         remaining_exp = temp_remaining_exp;
         remaining_ap  = temp_remaining_ap;
 
         $tbody.append(`
           <tr>
-            <td>Act ${mission.act}, Chapter ${mission.chapter}</td>
+            <td>Act ${mission.act}, Chapter ${mission.chapter}, ${mission.act_difficulty}</td>
             <td class="text-right">${mission.experience_ratio}</td>
             <td class="text-right">${remaining_exp} <span class="text-danger">(-${mission.experience_earned})</span></td>
             <td class="text-right">${remaining_ap} <span class="text-danger">(-${mission.ap_cost})</span></td>
           </tr>
-      `);
+        `);
+
+        exp_earned += mission.experience_earned;
+        ap_used    += mission.ap_cost;
 
         return false;
       });
+
+      loops -= 1;
+      console.log(loops);
     };
+
+    $tbody.append(`
+      <tr>
+        <td><strong>Total</strong></td>
+        <td class="text-right">-</td>
+        <td class="text-right"><strong>${exp_earned}</strong></td>
+        <td class="text-right"><strong>${ap_used}</strong></td>
+      </tr>
+    `);
   });
 });
