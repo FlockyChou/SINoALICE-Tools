ad2dfc60e1a48287aaa228acf98618b61627a81e
FlockyChou <flockychou@gmail.com>
2020-08-18 22:48:46 -0400

    Colosseum Dashboard: Add Situational Nightmares and Misc Info
    * Add option to add situation nightmares.
    * Add Shinma info.
    * Add Vanguard info.

--- assets/js/colosseum_dashboard.js
+++ assets/js/colosseum_dashboard.js
@@ -4,10 +4,15 @@ $(function() {
   let nightmares;
   let nightmare_options = [];
   let nightmare_selector_data;
+
+  let $nightmares_list;
   
-  const $nightmares          = $('#nightmares');
-  const $add_nightmare_modal = $('#add-nightmare');
-  const $nightmare_selector  = $('#nightmare-selector');
+  const $add_nightmare_button = $('.add-nightmare-btn');
+  const $add_nightmare_modal  = $('#add-nightmare');
+  const $nightmare_selector   = $('#nightmare-selector');
+
+  // Initialize static chosen fields
+  $('.chosen').chosen({ width: '100%' });
 
   // Get nightmare data from JSON
   $.getJSON('/data/colosseum_dashboard/nightmares.json', function(data) {
@@ -21,6 +26,12 @@ $(function() {
     nightmare_selector_data = nightmare_options.join('');
   });
 
+  $add_nightmare_button.click(function(e) {
+    console.log(this);
+    $nightmares_list = $(this).closest('.nightmares-list');
+    console.log($nightmares_list);
+  });
+
   $add_nightmare_modal.on('shown.bs.modal', function (e) {
     // On first time showing modal, add elements to dropdown
     if(first_modal_load) {
@@ -28,7 +39,7 @@ $(function() {
         // Add to summon order
 
         let $nightmare = $(generateNightmare(nightmares, e.target.value));
-        $nightmares.append($nightmare);
+        $nightmares_list.append($nightmare);
         $('.nightmare').popover({ html: true, trigger: 'hover' });
         
         // Clear Input and hide modal
@@ -50,29 +61,30 @@ function generateNightmare(nightmares, nightmare_name) {
   let effect_icons = [];
 
   $.each(nightmare.effects, function(index, effect) {
-    effect_icons.push(`<img class="nightmare-effect" src="/assets/img/effects/${effect}.png" alt="${effect}">`);
+    effect_icons.push(`<img class="nightmare-effect" src="/assets/img/effects/${effect}.png" alt="${effect}" draggable="false">`);
   });
 
   const nightmare_skill_name = nightmare.skill_name;
   const popover_data = `
+    <strong>${nightmare_skill_name}</strong>
     <p>${nightmare.description}</p>
     <p class='mb-0'>
-      SP: ${nightmare.sp_cost} <br/>
-      Charge Time: ${nightmare.charge_time_in_seconds} secs <br/>
-      Duration: ${nightmare.durationin_seconds} secs
+      <strong>SP:</strong> ${nightmare.sp_cost} <br/>
+      <strong>Charge Time:</strong> ${nightmare.charge_time_in_seconds} secs <br/>
+      <strong>Duration:</strong> ${nightmare.duration_in_seconds} secs
     </p>
   `;
 
   return `
-    <label class="card nightmare d-flex flex-row pull-left mr-3" data-toggle="popover" data-placement="bottom" data-title="${nightmare_skill_name}" data-content="${popover_data}">
+    <label class="card nightmare d-flex flex-row pull-left mr-3" data-toggle="popover" data-placement="bottom" data-title="${nightmare_name}" data-content="${popover_data}">
       <div class="nightmare-checkbox-container p-2 bg-light border-right">
         <input class="nightmare-checkbox" type="checkbox">
       </div>
       <div class="nightmare-image-container p-2">
-        <img class="nightmare-image" src="/assets/img/nightmares/${nightmare.image}" alt="${nightmare_name}">
+        <img class="nightmare-image" src="/assets/img/nightmares/${nightmare.image}" alt="${nightmare_name}" draggable="false">
         <div class="nightmare-effects text-center">${effect_icons.join('')}</div>
       </div>
     </label>
   </button>
   `;
-}
+ }
